dcc40f307482965ac1222ed703b6e37d307ab23d
NoelMacwan <yulespencer@gmail.com>
2015-05-07 12:56:02 +0530

    falcon: Tesla bringup
    Signed-off-by: NoelMacwan <yulespencer@gmail.com>

---
+++ tesla.mk
@@ -0,0 +1,7 @@
+$(call inherit-product, device/motorola/falcon/full_falcon.mk)
+
+# Inherit some common Tesla stuff.
+$(call inherit-product, vendor/tesla/config/common_full_phone.mk)
+
+PRODUCT_RELEASE_NAME := MOTO G
+PRODUCT_NAME := tesla_falcon
